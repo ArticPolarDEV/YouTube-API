39b5bb17021011f9c01cdfde42ef4f3341d39dca
ArticPolarDEV <vilma4040p@gmail.com>
2023-11-15 17:06:48 -0300

    Add files via upload

--- api.js
+++ api.js
@@ -6,14 +6,14 @@ var bodyParser = require('body-parser')
 require('dotenv').config();
 
 var app = express();
-var PORT = process.env.PORT || 80;
+// var PORT = process.env.PORT || 80;
 
 // Routers
 var getaudio = require('./routers/getaudio');
 var version = require('./routers/version');
 
 // all environments
-app.set('port', PORT)
+// app.set('port', PORT)
 app.use(favicon(path.join(__dirname, '/public/favicon.ico')))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')))
@@ -28,6 +28,7 @@ app.get('/', (req, res) => {
 app.use('/api/getaudio', getaudio);
 app.use('/api/version', version);
 
-app.listen(PORT, () => {
+module.exports = app;
+/* app.listen(PORT, () => {
   console.log('Express server listening on port ' + PORT);
-})
+}) */
